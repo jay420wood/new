54a96c739ac5f45286ecaa935bccb975439b8d47
jay420wood <jt@CGTMAED-iMac-38.local>
2015-01-07 08:34:18 -0700

    Initial Commit

---
+++ new2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  new2
+//
+//  Created by jt on 1/7/15.
+//  Copyright (c) 2015 jt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
